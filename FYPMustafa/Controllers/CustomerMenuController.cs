a3579e773b6423a57bb56f8ad173e0d538b01d07
Mu7tafa <mu7taf@gmail.com>
2019-01-08 12:31:41 +0800

    Customer Menu

---
+++ FYPMustafa/Controllers/CustomerMenuController.cs
@@ -0,0 +1,35 @@
+﻿using FYPMustafa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FYPMustafa.Controllers
+{
+    public class CustomerMenuController : Controller
+    {
+        private ApplicationDbContext _context;
+        public CustomerMenuController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: CustomerMenu
+        public ActionResult Menus(int id)
+        {
+            return View(_context.Menus.Where(c => c.RestaurantID == id).ToList());
+        }
+        public ActionResult MenuItems(int id)
+        {
+            return View(_context.MenuItems.Where(c => c.MenuID == id).ToList());
+        }
+        public ActionResult ItemDetail(int id)
+        {
+            return View(_context.MenuItems.SingleOrDefault(c => c.ItemID == id));
+        }
+    }
+}
